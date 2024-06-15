3b5033272f9916b81d3605df7c7469b385dfb67c
meehawk <meehawk@yahoo.com>
2024-06-15 03:43:18 -0400

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <div className="flex-1">
+        <h1>Hello</h1>
+      </div>
+    </main>
+  );
+}
