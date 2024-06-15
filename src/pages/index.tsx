036a5f5518db26d7288633f95aa9e63527dbbb92
meehawk <meehawk@yahoo.com>
2024-06-15 03:43:50 -0400

    chore: add doc links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,36 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import fsp from "node:fs/promises";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home({
+  documents,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="flex-1">
-        <h1>Hello</h1>
-      </div>
+    <main className={`p-3 space-y-3 ${inter.className}`}>
+      <h2 className="text-3xl font-semibold">Documents</h2>
+      <ul>
+        {documents.map((document) => (
+          <li key={document}>
+            <Link
+              href={`/documents/${document}`}
+              className="hover:underline cursor-pointer"
+            >
+              {document}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
+
+export const getServerSideProps = (async () => {
+  const documentsDir = "./public/documents";
+
+  const documents = await fsp.readdir(documentsDir);
+
+  return { props: { documents } };
+}) satisfies GetServerSideProps<{ documents: string[] }>;
